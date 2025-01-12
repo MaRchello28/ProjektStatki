d7db685eafe286a329628d7f0538d16e446e5df5
Orangunio <miloszdubowski@wp.pl>
2025-01-12 21:40:46 +0100

    special ship

--- Models/Creator/SpecialTypeShipCreator.cs
+++ Models/Creator/SpecialTypeShipCreator.cs
@@ -9,10 +9,36 @@ namespace ProjektStatki.Models.Creator
     public class SpecialTypeShipCreator
     {
         public SpecialTypeShipCreator() { }
-        public Ship createShip()
+        public Ship createShip(int type, string name) // type to jaki to statek ma byc
         {
-            SpecialShipType spec = new SpecialShipType("korweta");
-            return spec;
+            SpecialShipType ship = new SpecialShipType(name);
+            List<Point> points = new List<Point>();
+            switch (type)
+            {
+                case 0:
+                    {
+                        points.Add(new Point(0, 1));
+                        points.Add(new Point(0, 2));
+                        points.Add(new Point(1, 1));
+                        points.Add(new Point(0, 3));
+                        points.Add(new Point(1, 2));
+                        ship.points = points;
+                        return ship;
+                    }
+                case 1:
+                    {
+                        points.Add(new Point(0, 1));
+                        points.Add(new Point(0, 2));
+                        points.Add(new Point(1, 2));
+                        points.Add(new Point(2, 2));
+                        ship.points = points;
+                        return ship;
+                    }
+                default:
+                    {
+                        return ship;
+                    }
+            }
         }
     }
 }
