f1774daf840d367a985c60a3e711998170e57380
emdre <em.drewnowska@gmail.com>
2025-01-13 19:51:17 +0100

    proxy

--- Models/History/GameHistory.cs
+++ Models/History/GameHistory.cs
@@ -9,6 +9,7 @@ namespace ProjektStatki.Models.History
     public class GameHistory : History
     {
         public List<Game> gameHistory;
+
         public GameHistory(List<Game> g) { gameHistory = g; }
 
         public Game AddToGameHistory()
@@ -16,9 +17,12 @@ namespace ProjektStatki.Models.History
             return new Game();
         }
 
-        public void ShowHistory()
+        public void ShowHistory(List<Game> userGames)
         {
-
+            foreach (var game in userGames)
+            {
+                Console.WriteLine($"Game ID: {game.gameId}, Player 1: {game.player1.name}, Player 2: {game.player2.name}, Result: {game.result}");
+            }
         }
     }
 }
--- Models/History/GameHistoryProxy.cs
+++ Models/History/GameHistoryProxy.cs
@@ -8,15 +8,23 @@ namespace ProjektStatki.Models.History
 {
     public class GameHistoryProxy : History
     {
-        public GameHistory realhistory { get; set; }
-        public GameHistoryProxy(GameHistory realhistory)
+        private GameHistory realHistory; 
+        private int userId; 
+
+        public GameHistoryProxy(GameHistory realHistory, int userId)
         {
-            this.realhistory = realhistory;
+            this.realHistory = realHistory;
+            this.userId = userId;
         }
 
+        // Metoda, która ładuje historię gier tylko dla konkretnego użytkownika
         public void LoadHistory()
         {
-
+            if (realHistory.gameHistory != null)
+            {
+                var userGames = realHistory.gameHistory.Where(game => game.player1.playerId == userId || game.player2.playerId == userId).ToList(); //tu krzyczy bo nie ma zadnego id do przyjecia (chyba)
+                ShowHistory(userGames);
+            }
         }
 
         public Game AddToGameHistory()
@@ -24,9 +32,12 @@ namespace ProjektStatki.Models.History
             return new Game();
         }
 
-        public void ShowHistory()
+        public void ShowHistory(List<Game> userGames)
         {
-
+            foreach (var game in userGames)
+            {
+                Console.WriteLine($"Game ID: {game.gameId}, Player 1: {game.player1.name}, Player 2: {game.player2.name}, Result: {game.result}");
+            }
         }
     }
 }
