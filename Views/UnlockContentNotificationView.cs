d2a49c3b9a25e2ccfe1df3d9f4d221652e5d96fd
Orangunio <miloszdubowski@wp.pl>
2025-01-19 14:46:53 +0100

    Widoki dla Contentu

---
+++ Views/UnlockContentNotificationView.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjektStatki.Views
+{
+    public partial class UnlockContentNotificationView : Form
+    {
+        public UnlockContentNotificationView(string contentName)
+        {
+            InitializeComponent();
+            label1.Text = $"Odblokowałeś: {contentName}";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
